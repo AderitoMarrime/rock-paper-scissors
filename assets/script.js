0c209b6d33c58524a669f532ab7920cc49405555
Adérito Marrime <153687419+AderitoMarrime@users.noreply.github.com>
2025-03-24 12:28:08 +0200

    feat: add a function to generate a random choice
    the function can generate 3 choices, rock, paper or scissors

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+function getComputerChoice () {
+    let number = Math.floor( Math.random() * 3)
+    let choice = null
+    if (number === 0) {
+        choice = "rock"
+    } else if (number === 1) {
+        choice = "paper"
+    } else {
+        choice = "scissors"
+    }
+    return choice
+}
+
+let a = getComputerChoice();
+
+console.log(a)
